a8cb747ec5f1bf47b8bd5286bc586a89eb4cdd3c
mimi <mskliarova@sfedu.ru>
2025-03-07 00:53:59 +0300

    Добавление контента, замена навбара на хедер и добавление футера

--- my-app/src/Page1.jsx
+++ my-app/src/Page1.jsx
@@ -1,20 +1,33 @@
 import React, { useState } from 'react';
 
 function Page1() {
-  const [count, setCount] = useState(0);
-  async function name() {
+  const [inputString, setInputString] = useState('');
+  const [camelCaseString, setCamelCaseString] = useState('');
+
+  async function navigateToPage2() {
     document.location.href = "http://localhost:3000/Page2";
   }
 
+  function toCamelCase(str) {
+    str = str.replace(/[-_]/g, ' ');
+    const words = str.split(' ');
+    // Convert words to camelCase
+    return words[0] + words.slice(1).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
+  }
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setInputString(value);
+    setCamelCaseString(toCamelCase(value));
+  };
+
   return (
     <div className="write">
-      <center><h1>Страница 1</h1></center>
-      <h1>Счётчик: {count}</h1>
-      <button className='button_text' onClick={() => setCount(count + 1)}>Увеличить</button>
-      <button className='button_text' onClick={() => setCount(count - 1)}>Уменьшить</button>
-      <br />
-
-      <button className='button_text' onClick={() => name()}>Страница 2</button>
+      <center><h1>Страница 1</h1></center><br/>
+      <p>Задание 1.4:<br/>
+      Пример: the-stealth-warrior - theStealthWarrior</p>
+      <input onChange={handleChange} /> = {camelCaseString}<br/><br/>
+      <button className='button_text' onClick={navigateToPage2}>Переход на страницу 2</button>
     </div>
   );
 }
--- my-app/src/Page2.jsx
+++ my-app/src/Page2.jsx
@@ -21,64 +21,15 @@ function Page2() {
 
   return (
     <div className={color}>
-      {/* Навигационная панель с бургер-меню */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <a className="navbar-brand" href="#">Мой сайт</a>
-          
-          {/* Кнопка для бургер-меню на мобильных устройствах */}
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <br />
+      <a className="nav-link" href="#" onClick={goToPage1}>Переход на страницу 1</a><br />
 
-          {/* Контекстное меню */}
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Главная</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">О нас</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#" onClick={goToPage1}>На страницу 1</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      {/* Кнопка для смены темы */}
       <button className="row" onClick={toggleTheme}>
         Сменить тему
       </button>
 
-      {/* Заголовок страницы */}
       <div className="text-center fadeIn">
         <h1 className="my-4">Страница 2</h1>
-      </div>
-
-      {/* Контейнер с текстом и кнопкой */}
-      <div className="container">
-        <h2 className="text-primary text-center">Привет, мир!</h2>
-        <div className="d-flex justify-content-center">
-          <button className="btn btn-success">Кнопка</button>
-        </div>
-      </div>
-
-      {/* Сетка с тремя равными колонками */}
-      <div className="container my-4">
-        <div className="row mt-4">
-          <div className="col-12 col-md-4">
-            <div className="p-3 bg-light border">1</div>
-          </div>
-          <div className="col-12 col-md-4">
-            <div className="p-3 bg-light border">2</div>
-          </div>
-          <div className="col-12 col-md-4">
-            <div className="p-3 bg-light border">3</div>
-          </div>
-        </div>
       </div>
     </div>
   );
